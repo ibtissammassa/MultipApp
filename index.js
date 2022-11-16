b8c95c0c4071ee80c8d4f4fde86650c80e26106a
ibtissammassa <ibtissammassa16@gmail.com>
2022-11-16 21:37:19 +0100

    Js code

---
+++ index.js
@@ -0,0 +1,40 @@
+const num1 = Math.ceil(Math.random()*10);
+const num2 = Math.ceil(Math.random()*10);
+
+const a = document.getElementById("a");
+const b = document.getElementById("b");
+const scoreEl = document.getElementById("score");
+const inputEl = document.getElementById("input");
+const formEl = document.getElementById("form");
+
+const Answer = num1*num2;
+
+a.innerText = num1;
+b.innerText = num2;
+
+// get score from storage
+var score = +(localStorage.getItem("score"));
+if(!score){
+    score=0;
+}
+
+scoreEl.innerText=score;
+
+// main function
+formEl.addEventListener("submit",
+()=>{
+    const userAns = +inputEl.value;
+    if(userAns==Answer){
+        score++;
+        UpdateLocalStorage();
+    }else{
+        score--;
+        UpdateLocalStorage();
+    }
+}
+)
+
+//storing the score
+function UpdateLocalStorage(){
+    localStorage.setItem("score",score.toString());
+}
